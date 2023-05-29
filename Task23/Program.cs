6fe714f76e229d765f38cd72dffa1c73590205e2
Reezo1986 <Reezo1986@gmail.com>
2023-05-29 15:41:45 +0600

    Задача №23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,7 +1,15 @@
-﻿Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= number; i++)
+﻿int Prompt(string message)
 {
-    Console.Write(Math.Pow(i, 3) + "\t"); 
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Int32.Parse(value);
+    return result;
 }
 
+double thirdDeg;
+int number = Prompt("Введите число: ");
+for (int i = 1; i <= number; i++)
+{
+    thirdDeg = Math.Pow(i, 3);
+    Console.Write($"{i} в 3 степени равно: {thirdDeg}  \t"); 
+}
