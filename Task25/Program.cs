132604cb53940a7b3d0c4ea843a00dcc0e10a20f
Reezo1986 <Reezo1986@gmail.com>
2023-06-01 15:31:27 +0600

    Задача №25

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int NumberDeg(int number, int n)
+{
+	int res = number;
+	for (int i = 1; i < n; i++)
+	{
+		res *= number;
+	}
+    Console.WriteLine($"Число {number} в степени {n} равно: {res}");
+	return res;
+}
+
+NumberDeg(5, 2);
