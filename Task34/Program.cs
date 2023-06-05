56081adf3f21fb4ab227c555e0eccf9d4b2311b3
Reezo1986 <Reezo1986@gmail.com>
2023-06-05 15:00:07 +0600

    Задача №34

---
+++ Task34/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] resultArray = new int[size];
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+int[] FindEvenNum(int[] arr){
+    int evenNumbers = 0;
+    int countOfEvenNum = 0;
+    Console.WriteLine("В данном массиве четными числами являются: ");
+    for(int i = 0; i < arr.Length; i++)
+    if (arr[i] % 2 == 0)
+    {
+        evenNumbers = arr[i];
+        countOfEvenNum = countOfEvenNum + 1;
+        Console.WriteLine(evenNumbers);
+    }
+    if (countOfEvenNum == 1){
+       Console.WriteLine($"В данном массиве {countOfEvenNum} четное число");  // Условный оператор if расчитан не на все случаи
+    }
+    else if (countOfEvenNum > 1 && countOfEvenNum < 5){
+        Console.WriteLine($"В данном массиве {countOfEvenNum} четных числа");
+    }
+    else{
+        Console.WriteLine($"В данном массиве {countOfEvenNum} четных чисел");
+    }
+    return arr;
+}
+
+int[] array = GetArray(10, 100, 999);
+Console.WriteLine($"Массив: [ {String.Join("; ", array)} ]");
+int[] arr = FindEvenNum(array);
+
