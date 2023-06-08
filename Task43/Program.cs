4b3018a022de23553a0eed241d6c40add6cd43c2
Reezo1986 <Reezo1986@gmail.com>
2023-06-08 19:30:44 +0600

    Задача№43

---
+++ Task43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите значение b1");
+double b1 = Int32.Parse((Console.ReadLine()));
+Console.WriteLine("введите число k1");
+double k1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Int32.Parse(Console.ReadLine());
+
+double x;
+double y;
+
+x = (b2 - b1)/(k1 - k2);
+y = k1 * x + b1;
+
+Console.WriteLine($"Прямые пересекутся в точке с координатами X: {x}, Y: {y}");
