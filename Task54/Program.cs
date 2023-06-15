3eafc1f5ac9f52675b05dbfad1010909cada13bd
Reezo1986 <Reezo1986@gmail.com>
2023-06-16 04:55:45 +0600

    Задача №54

---
+++ Task54/Program.cs
@@ -0,0 +1,60 @@
+﻿// //Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] GetArray2d(int m, int n, int min, int max)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array2d = GetArray2d(rows, columns, 0, 10);
+Console.WriteLine("Исходная матрица:");
+PrintMatrix(array2d);
+SortOfArray(array2d);
+Console.WriteLine();
+Console.WriteLine("Отсортированная матрица по строкам:");
+PrintMatrix(array2d);
+
+void SortOfArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    double temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
