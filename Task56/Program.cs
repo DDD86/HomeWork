e5e13615fda75fb73554f78a3f5a0be4c1234fea
Reezo1986 <Reezo1986@gmail.com>
2023-06-16 08:18:40 +0600

    Задача №56

---
+++ Task56/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] matrix = GetMatrix(4, 4, 10);
+
+PrintMatrix(matrix);
+
+int[,] GetMatrix(int m, int n, int value)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumOfArrayRows(int[,] array)
+{
+    int min = int.MaxValue;
+    int line = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = array[i, 0];
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+
+            sum += array[i, j];
+        }
+        if (sum < min)
+        {
+            min = sum;
+            line = i + 1;
+        }
+    }
+    Console.WriteLine($"Наименьшая сумма: [{min}], находится в строке: [{line}]");
+}
+
+SumOfArrayRows(matrix);
+
+
+
