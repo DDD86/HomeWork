b0e9871d31f8f17bb3c8f644ac1526d1ec05029d
Reezo1986 <Reezo1986@gmail.com>
2023-06-16 08:40:58 +0600

    Задача №58

---
+++ Task58/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] matrix = GetMatrix(3, 3, 10);
+int[,] matrix2 = GetMatrix(3, 3, 10);
+int[,] resultMatrix = new int[3, 3];
+
+PrintMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+
+int[,] GetMatrix(int m, int n, int value)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+      Console.Write(resultMatrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+MultiplyMatrix(matrix, matrix2, resultMatrix);
